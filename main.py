bc65dbd1332a818c158c21759ee336b2bc9569fb
Black Phin <shivanshgoel009@gmail.com>
2021-09-23 02:45:14 +0530

    Update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import xlrd, pyautogui, time, datetime, os, csv
+import xlrd, time, datetime, os, csv
+import pyautogui as auto
 
 time_slots = 8
 
@@ -7,51 +8,65 @@ time_slots = 8
 wb1 = xlrd.open_workbook(
     r"D:\OneDrive\Repositories\Zoom-Automatic-Login-v2\Credentials.xls"
 )
-wb2 = xlrd.open_workbook(
+wb2 = xlrd.open_workbook(r"D:\OneDrive\Repositories\Zoom-Automatic-Login-v2\Mode.xls")
+wb3 = xlrd.open_workbook(
     r"D:\OneDrive\Repositories\Zoom-Automatic-Login-v2\Timetable.xls"
 )
 sheet_credential = wb1.sheet_by_index(0)
-sheet_timetable = wb2.sheet_by_index(0)
+sheet_mode = wb2.sheet_by_index(0)
+sheet_timetable = wb3.sheet_by_index(0)
 sheet_credential.cell_value(0, 0)
+sheet_mode.cell_value(0, 0)
 sheet_timetable.cell_value(0, 0)
 
 
 # __functions__
-def launch():
-    ...
-
-
-def auto_join(id, passw):
-    pyautogui.FAILSAFE = False
-    pyautogui.hotkey("win", "d")
+def start_zoom(x=0):
+    auto.FAILSAFE = False
+    if x == 1:
+        time.sleep(0.5)
+        auto.hotkey("ctrl", "win", "d")
     time.sleep(0.5)
-    pyautogui.press("win")
+    auto.press("win")
     time.sleep(0.5)
-    pyautogui.write(message="start zoom")
+    auto.write(message="start zoom")
     time.sleep(0.5)
-    pyautogui.press("enter")
-    time.sleep(1)
-    pyautogui.click(x=956, y=573)
-    time.sleep(0.5)
-    pyautogui.click(x=1155, y=518)
+    auto.press("enter")
     time.sleep(5)
-    pyautogui.hotkey("win", "up")
+    auto.hotkey("win", "up")
+
+
+def auto_join(id="977 0864 8127", passw="261893"):
+    auto.click(x=776, y=433)
     time.sleep(0.5)
-    pyautogui.click(x=717, y=57)
-    time.sleep(0.5)
-    pyautogui.click(x=774, y=425)
-    time.sleep(1)
-    pyautogui.write(message=id)
-    time.sleep(0.5)
-    pyautogui.press("enter")
+    auto.click(x=769, y=466)
+    auto.write(message=id)
+    auto.press("enter")
     time.sleep(2)
-    pyautogui.write(message=passw)
-    time.sleep(0.5)
-    pyautogui.press("enter")
-    time.sleep(20)
-    pyautogui.hotkey("win", "up")
-    time.sleep(0.5)
-    pyautogui.hotkey("win", "d")
+    auto.click(x=769, y=466)
+    auto.write(message=passw)
+    auto.press("enter")
+
+
+def kill_zoom(x=0):
+    os.system("taskkill /IM zoom.exe /T /F")
+    auto.moveTo(1410, 1057)
+    auto.moveTo(1700, 1057)
+    auto.moveTo(1920 / 2, 1080 / 2)
+    if x == 1:
+        auto.hotkey("ctrl", "win", "f4")
+
+
+def wait_till(timea):
+    today = datetime.date.today()
+    dt = datetime.datetime.strptime(timea, "%H:%M")
+    when = datetime.datetime(*today.timetuple()[:3], *dt.timetuple()[3:6])
+    wait_time = (when - datetime.datetime.now()).total_seconds()
+    if wait_time < 0:
+        print(f"Time {when} has already passed")
+    else:
+        print(f"Waiting {wait_time} seconds until {when}")
+        time.sleep(wait_time)
 
 
 time_start = []
@@ -66,25 +81,79 @@ def time_list():
 
 
 subject_list = []
+day_index = 0
+day = datetime.datetime.today().strftime("%A")
 
 
-def subjects_today():
-    today_day = datetime.datetime.today().strftime("%A")
+def subjects_today(today_day=day):
+    global subject_list, day_index
+    # today_day = datetime.datetime.today().strftime("%A")
     for x in range(sheet_timetable.nrows):
         if today_day == sheet_timetable.cell_value(x, 0):
             list = sheet_timetable.row_values(x)[1:]
-            for x in list:
-                if list.count(x) != 1 and x != "":
-                    list[list.index(x) + 1] = ""
+            for y in list:
+                if list.count(y) != 1 and y != "" and y != "R":
+                    list[list.index(y) + 1] = "R"
             subject_list.extend(list)
+            day_index = x
+            break
+
+
+subject_mode = []
+
+
+def mode():
+    global subject_list, subject_mode
+    subject_mode = list(subject_list)
+    for z in subject_mode:
+        if z != "" and z != "R":
+            subject_mode[subject_mode.index(z)] = [
+                z,
+                sheet_mode.cell_value(day_index, subject_mode.index(z) + 1),
+            ]
+
+
+credentials_list = []
+
+
+def credentials():
+    global credentials_list, subject_mode
+    for v in subject_mode:
+        if v != "":
+            for i in range(sheet_credential.nrows):
+                if v[0] == sheet_credential.cell_value(i, 0):
+                    row_number = i
+                    if v[1] == 1:
+                        column_number = 1
+                    elif v[1] == 2:
+                        column_number = 3
+                    credentials_list.append(
+                        [
+                            sheet_credential.cell_value(row_number, column_number),
+                            str(
+                                int(
+                                    sheet_credential.cell_value(
+                                        row_number, column_number + 1
+                                    )
+                                )
+                            ),
+                        ]
+                    )
+        elif v == "":
+            credentials_list.append("")
 
 
 # __main__
 time_list()
-print(time_start, time_end)
+print(time_start)
+print(time_end)
 
 subjects_today()
 print(subject_list)
+print(day_index)
 
+mode()
+print(subject_mode)
 
-# datetime.datetime.strptime(time, "%H%M")
+credentials()
+print(credentials_list)
